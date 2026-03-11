ea79d67684dd6fcda9154cbe586bdaadee1dda51
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-03-10 23:54:15 -0300

    updated menu styles dark/light

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -414,10 +414,16 @@ const Header = ({ isAuthenticated, user, userRole, onLogout, isLoading }: Header
                 Registrarse
               </Link>
             </div>
-            <div className="w-full flex justify-between">
+            <div className="w-full flex justify-between items-center">
               <span>Apariencia</span>
-              <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                {theme === "dark" ? <Sun size={18} /> : <Moon size={18} />}
+              <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                className="w-[58px] border dark:border-zinc-800 border-zinc-300 rounded-2xl flex items-center bg-[#eee] dark:bg-[#111111]"
+                >
+                <span className="p-1 rounded-full bg-background border border-zinc-300 dark:border-zinc-800 text-muted-foreground m-0.5 transition-transform duration-300 shadow-2xs"
+                style={{ transform: `${theme === "dark" ? "translateX(0)" : "translateX(100%)"}` }}
+                >
+                  {theme === "dark" ? <Sun size={16} /> : <Moon size={16} />}
+                </span>
               </button>
             </div>
             <div className="absolute bottom-0 left-0 w-full h-16 border-t border-border">
