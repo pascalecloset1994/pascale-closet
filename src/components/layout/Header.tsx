46c03905e8960dbb3ffb35a2be71a15beb0cb3cd
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-03-11 01:00:57 -0300

    updated switch

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -383,13 +383,18 @@ const Header = ({ isAuthenticated, user, userRole, onLogout, isLoading }: Header
                   : null}
               </div>
             )}
-            <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-              {theme === "dark" ? (
-                <Sun size={15} />
-              ) : (
-                <Moon size={15} />
-              )}
-            </button>
+            <div className="w-full flex justify-between items-center">
+              <span>Apariencia</span>
+              <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                className="w-[58px] border dark:border-zinc-800 border-zinc-300 rounded-2xl flex items-center bg-[#eee] dark:bg-[#111111]"
+              >
+                <span className="p-1 rounded-full bg-background border border-zinc-300 dark:border-zinc-800 text-muted-foreground m-0.5 transition-transform duration-300 shadow-2xs"
+                  style={{ transform: `${theme === "dark" ? "translateX(0)" : "translateX(100%)"}` }}
+                >
+                  {theme === "dark" ? <Sun size={16} /> : <Moon size={16} />}
+                </span>
+              </button>
+            </div>
             <button
               onClick={() => {
                 onLogout();
